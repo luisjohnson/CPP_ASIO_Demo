0dc8701ab855b8728949d422879f97dcfadb7499
Luis Johnson <luisjohnson@me.com>
2022-01-24 11:41:01 -0800

    Handling an exception

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,36 @@
 #include <algorithm>
 
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
 std::mutex global_stream_lock;
 
 void WorkerThread(const std::shared_ptr<boost::asio::io_service>& service, int counter)
 {
     global_stream_lock.lock();
-    std::cout << "Thread " << counter << std::endl;
+    std::cout << "Thread " << counter << " Start." << std::endl;
     global_stream_lock.unlock();
 
-    service->run();
-
-    global_stream_lock.lock();
-    std::cout << "Thread " <<  counter << std::endl;
-    global_stream_lock.unlock();
+    try {
+        service->run();
+        global_stream_lock.lock();
+        std::cout << "Thread " <<  counter << std::endl;
+        global_stream_lock.unlock();
+    }
+    catch(std::exception &exception)
+    {
+        global_stream_lock.lock();
+        std::cout << "Message: " <<  exception.what() << std::endl;
+        global_stream_lock.unlock();
+    }
 }
 
-void PrintNumber(int number)
+void ThrowAnException(const std::shared_ptr<boost::asio::io_service>& service, int counter)
 {
-    std::cout << "Number :" << number << std::endl;
+    global_stream_lock.lock();
+    std::cout << "Throw Exception : " << counter << std::endl;
+    global_stream_lock.unlock();
+    throw(std::runtime_error("The exception!!"));
 }
 
 int main()
@@ -38,25 +49,23 @@ int main()
 
     std::vector<std::thread> threads;
 
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 2; i++) {
         threads.emplace_back([ioService, i] {
             return WorkerThread(ioService, i);
         });
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    ioService->post(strand.wrap([] { return PrintNumber(1); }));
-    ioService->post(strand.wrap([] { return PrintNumber(2); }));
+    //  We can also see that although we post work for the io_service object five times,
+    //  the exception handling only handle two exceptions because once the thread has finished,
+    //  the join_all() function in the thread will finish the thread and exit the program.
+    //  In other words, we can say that once the exception is handled, the thread exits to join the call.
+    `//  Additional code that might have thrown an exception will never be called.
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    ioService->post(strand.wrap([] { return PrintNumber(3); }));
-    ioService->post(strand.wrap([] { return PrintNumber(4); }));
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    ioService->post(strand.wrap([] { return PrintNumber(5); }));
-    ioService->post(strand.wrap([] { return PrintNumber(6); }));
-
-    worker.reset();
+    ioService->post([ioService] { return ThrowAnException(ioService, 1); });
+    ioService->post([ioService] { return ThrowAnException(ioService, 2); });
+    ioService->post([ioService] { return ThrowAnException(ioService, 3); });
+    ioService->post([ioService] { return ThrowAnException(ioService, 4); });
+    ioService->post([ioService] { return ThrowAnException(ioService, 5); });
 
     std::for_each(threads.begin(), threads.end(), [](std::thread &thread) {
         thread.join();
